afebb2e6a1ade84c23dfff8c6c1357e06496931a
Prithviraj Kalaiselvan <prithviraj792000@gmail.com>
2024-01-31 10:52:06 -0500

    Setup Error Handling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,36 +7,34 @@ import SortSelector from "../components/SortSelector";
 
 function HomePage() {
   return (
-    <>
-      <Grid
-        templateAreas={{
-          base: ` "main"`,
-          lg: ` "aside main"`,
-        }}
-        templateColumns={{
-          base: "1fr",
-          lg: "250px 1fr",
-        }}
-      >
-        <Show above="lg">
-          <GridItem area="aside" paddingX={5}>
-            <GenreList />
-          </GridItem>
-        </Show>
-        <GridItem area="main">
-          <Box paddingLeft={2}>
-            <GameHeading />
-            <Flex marginBottom={5}>
-              <Box marginRight={5}>
-                <PlatformSelector />
-              </Box>
-              <SortSelector />
-            </Flex>
-          </Box>
-          <GameGrid />
+    <Grid
+      templateAreas={{
+        base: ` "main"`,
+        lg: ` "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "250px 1fr",
+      }}
+    >
+      <Show above="lg">
+        <GridItem area="aside" paddingX={5}>
+          <GenreList />
         </GridItem>
-      </Grid>
-    </>
+      </Show>
+      <GridItem area="main">
+        <Box paddingLeft={2}>
+          <GameHeading />
+          <Flex marginBottom={5}>
+            <Box marginRight={5}>
+              <PlatformSelector />
+            </Box>
+            <SortSelector />
+          </Flex>
+        </Box>
+        <GameGrid />
+      </GridItem>
+    </Grid>
   );
 }
 
